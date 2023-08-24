84d50a4fd3ffb43ad0e705a258950e29ab3c0dbe
“v1n1c1u5ouza” <viniciusdesouzamarinho@gmail.com>
2023-08-24 14:44:00 -0300

    execise to study if

---
+++ if_exercise.py
@@ -0,0 +1,7 @@
+first_value = input('Type the first value:')
+second_value = input('Type the second value:')
+
+if first_value >= second_value:
+  print(f'{first_value=} is bigger or equal than the {second_value=}')
+else:
+  print(f'{second_value=} is bigger than the {first_value=}')
