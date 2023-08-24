8820a061ebedcb2b09176ea42da65ca5d8467407
“v1n1c1u5ouza” <viniciusdesouzamarinho@gmai.com>
2023-08-24 14:20:45 -0300

    Calculator that calculate the bmi fo the person using varibles

---
+++ bmi_calculator.py
@@ -0,0 +1,12 @@
+name = 'Matheus Luiz'
+height = 1.80
+weight = 95
+bmi = weight / height ** 2
+line_1 = f'{name} is {height} tall,'
+line_2 = f'weighs {weight} and his BMI is'
+line_3 = f'{bmi:.2f}'
+
+print(line_1)
+print(line_2)
+print(line_3)
+
